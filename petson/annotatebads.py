f4a8a28f2b7a06a87f4491c3ed0dbbffa31b9c64
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2022-06-09 00:00:47 +0300

    petson

--- petson/annotatebads.py
+++ petson/annotatebads.py
@@ -13,34 +13,38 @@ metapath = '/home/ariuna/rafaello/viadastra/additional/metadata_v5.tsv'
 def group_by_bad(path_tsv, path_badmap, out_path):
     #path_bed - badmap path
     #path_vcf - before babachi tsv
-    header_list = ['#CHROM', 'POS','POS2', 'ID', 'REF', 'ALT', 'ref', 'alt']
-    #vcf_data_atac = pd.read_csv(os.path.join(processed_data,'pulled_atacseq_tobabachi.vcf'),sep='\t', names = header_list)
-    vcf_data = pd.read_csv(os.path.join(bedfiles, path_tsv), sep='\t',
-                                names=header_list)
-    vcf_data['POS2'] = vcf_data['POS']
-    vcf_data = vcf_data[['#CHROM', 'POS', 'POS2','ID', 'REF', 'ALT', 'ref', 'alt']]
-    vcf_list = vcf_data.values.tolist()
-    test = BedTool(vcf_list)
+    try:
+        header_list = ['#CHROM', 'POS','POS2', 'ID', 'REF', 'ALT', 'ref', 'alt']
+        #vcf_data_atac = pd.read_csv(os.path.join(processed_data,'pulled_atacseq_tobabachi.vcf'),sep='\t', names = header_list)
+        vcf_data = pd.read_csv(os.path.join(bedfiles, path_tsv), sep='\t',
+                                    names=header_list)
+        vcf_data['POS2'] = vcf_data['POS']
+        vcf_data = vcf_data[['#CHROM', 'POS', 'POS2','ID', 'REF', 'ALT', 'ref', 'alt']]
+        vcf_list = vcf_data.values.tolist()
+        test = BedTool(vcf_list)
 
-    #bed_data = pd.read_csv(os.path.join(processed_data,'pulled_atacseq_tobabachi.bed'),sep='\t')
-    bed_data = pd.read_csv(os.path.join(processed_data, path_badmap), sep='\t')
-    bed_data = bed_data[['#chr','start','end','BAD']]
-    bed_data = bed_data[bed_data.end>=bed_data.start]
-    bed_list = bed_data.values.tolist()
+        #bed_data = pd.read_csv(os.path.join(processed_data,'pulled_atacseq_tobabachi.bed'),sep='\t')
+        bed_data = pd.read_csv(os.path.join(processed_data, path_badmap), sep='\t')
+        bed_data = bed_data[['#chr','start','end','BAD']]
+        bed_data = bed_data[bed_data.end>=bed_data.start]
+        bed_list = bed_data.values.tolist()
 
-    annotations = BedTool(bed_list)
-    i = test.intersect(annotations, wb=True)
-    df = i.to_dataframe()
-    print(df.iloc[0,:])
-    df.columns = ['#CHROM', 'POS','POS2','ID', 'REF', 'ALT', 'REF_COUNTS', 'ALT_COUNTS','chr','start','end','BAD']
+        annotations = BedTool(bed_list)
+        i = test.intersect(annotations, wb=True)
+        df = i.to_dataframe()
+        #print(df.iloc[0,:])
+        df.columns = ['#CHROM', 'POS','POS2','ID', 'REF', 'ALT', 'REF_COUNTS', 'ALT_COUNTS','chr','start','end','BAD']
 
-    #annotated_vcf = annotated_vcf[(annotated_vcf.ref >= threshold) & (annotated_vcf.alt >=threshold)]
-    #annotated_vcf = annotated_vcf[((annotated_vcf.ref + annotated_vcf.alt) >= threshold)]
-    df['POS2'] = df['POS']+1
-    annotated_vcf = df[['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'REF_COUNTS', 'ALT_COUNTS', 'BAD']]
-    annotated_vcf.to_csv(os.path.join(bedfiles, out_path+ '_annotated.tsv'),header=True,
-                        index=False, sep='\t')
-
+        #annotated_vcf = annotated_vcf[(annotated_vcf.ref >= threshold) & (annotated_vcf.alt >=threshold)]
+        #annotated_vcf = annotated_vcf[((annotated_vcf.ref + annotated_vcf.alt) >= threshold)]
+        df['POS2'] = df['POS']+1
+        annotated_vcf = df[['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'REF_COUNTS', 'ALT_COUNTS', 'BAD']]
+        annotated_vcf.to_csv(os.path.join(bedfiles, out_path+ '_annotated.tsv'),header=True,
+                            index=False, sep='\t')
+    except Exception:
+        with open('scorefiles/logs', "a") as log:
+            log.write('failed ' + i + '\t' + Exception +'\n')
+        print(Exception)
 
 
 if(not os.path.isdir(os.path.join(bedfiles,'scorefiles'))):
