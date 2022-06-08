d5283298885cbe0c55fd54ef00c3d3bfaa7bcec9
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2022-06-08 22:16:22 +0300

    petson

--- petson/group_by_bad.py
+++ petson/group_by_bad.py
@@ -46,7 +46,7 @@ def group_by_bad(path_tsv, path_badmap, out_path):
         temp_bad = annotated_vcf[annotated_vcf.BAD == (i + 1)]
         temp_bad = temp_bad.drop(['BAD'],axis=1)
         df_list.append(temp_bad)
-        if (os.path.exists(os.path.join(processed_data, out_path + '/BAD' + str(i + 1))) == False):
+        if (os.path.exists(os.path.join(processed_data, out_path + '/BAD' + str(i + 1) +'.00')) == False):
             os.mkdir(os.path.join(processed_data, out_path + '/BAD' + str(i + 1)+'.00'))
         temp_bad.to_csv(os.path.join(processed_data, out_path + '/BAD' + str(i + 1) +'.00'+ '/table.tsv'), header=True,
                         index=False, sep='\t')
