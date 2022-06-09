4a7fa7a57648915f0025a2c79e4e9b73aeb9623b
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2022-06-09 18:52:32 +0300

    petson

--- petson/group_by_bad.py
+++ petson/group_by_bad.py
@@ -23,15 +23,15 @@ def group_by_bad(path_tsv, path_badmap, out_path):
 
     #bed_data = pd.read_csv(os.path.join(processed_data,'pulled_atacseq_tobabachi.bed'),sep='\t')
     bed_data = pd.read_csv(os.path.join(processed_data, path_badmap), sep='\t')
-    bed_data = bed_data[['#chr','start','end','BAD']]
+    bed_data = bed_data[['#chr','start','end']]
     bed_data = bed_data[bed_data.end>=bed_data.start]
     bed_list = bed_data.values.tolist()
 
     annotations = BedTool(bed_list)
     i = test.intersect(annotations, wb=True)
     df = i.to_dataframe()
-    df.columns = ['#CHROM', 'POS','POS2','ID', 'REF', 'ALT', 'ref', 'alt','chr','start','end','BAD']
-
+    df.columns = ['#CHROM', 'POS','POS2','ID', 'REF', 'ALT', 'ref', 'alt','chr','start','end']
+    df = pd.merge(df, bed_data[['#chr', 'start', 'end', 'BAD']], how='left', on=['#chr','start', 'end'])
     #annotated_vcf = annotated_vcf[(annotated_vcf.ref >= threshold) & (annotated_vcf.alt >=threshold)]
     #annotated_vcf = annotated_vcf[((annotated_vcf.ref + annotated_vcf.alt) >= threshold)]
     if (os.path.exists(os.path.join(processed_data, out_path)) == False):
