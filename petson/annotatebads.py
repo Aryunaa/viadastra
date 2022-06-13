54b8bf6d7ef14e26f522587e225d6ede542e6bfd
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2022-06-13 20:00:45 +0300

    petson

--- petson/annotatebads.py
+++ petson/annotatebads.py
@@ -23,20 +23,20 @@ def annotate_by_bad(path_tsv, path_badmap, out_path):
         vcf_data = vcf_data[['#CHROM', 'POS', 'POS2','ID', 'REF', 'ALT', 'ref', 'alt']]
         vcf_list = vcf_data.values.tolist()
         test = BedTool(vcf_list)
-
+        print(vcf_data.shape[0])
         #bed_data = pd.read_csv(os.path.join(processed_data,'pulled_atacseq_tobabachi.bed'),sep='\t')
         bed_data_old = pd.read_csv(os.path.join(processed_data, path_badmap), sep='\t')
         bed_data = bed_data_old[['#chr','start','end']]
         bed_data = bed_data[bed_data.end>=bed_data.start]
         bed_list = bed_data.values.tolist()
-
+        print(bed_data.shape[0])
         annotations = BedTool(bed_list)
         i = test.intersect(annotations, wb=True)
         df = i.to_dataframe()
         #print(df.iloc[0,:])
         df.columns = ['#CHROM', 'POS', 'POS2', 'ID', 'REF', 'ALT', 'ref', 'alt', '#chr', 'start', 'end']
         df = pd.merge(df, bed_data_old[['#chr', 'start', 'end', 'BAD']], how='left', on=['#chr', 'start', 'end'])
-
+        print(df.shape[0])
         #annotated_vcf = annotated_vcf[(annotated_vcf.ref >= threshold) & (annotated_vcf.alt >=threshold)]
         #annotated_vcf = annotated_vcf[((annotated_vcf.ref + annotated_vcf.alt) >= threshold)]
         df['POS2'] = df['POS']+1
