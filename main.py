78bf5e776be3c743331aec566b55bb6f9d0c8a52
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2022-10-28 17:11:01 +0300

    i

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ from steps.step1_mk_links import *
 
 parser = argparse.ArgumentParser(description='viadastra pipeline, perfoms snp-calling, filtrating and works with babachi and mixalime')
 parser.add_argument("-c","--config", help="Path for cfg file, which contains all parameters", required=True)
+helpm = "Define a processing step please.\n1 creates soft links and directories\n2.1 for reference processing"\
+        + "\n2.2.1 snp calling for one file\n2.2 snp calling for all files "
+
 parser.add_argument("-s","--step",
-        help="Define a processing step please.\n1 creates soft links and directories\n2.1 for reference processing" +
-        "\n2.2.1 snp calling for one file\n2.2 snp calling for all files "
+        help=helpm
                     , required=True)
 parser.add_argument("-j","--jobs", help="Number of jobs", default='4', required=False)
 parser.add_argument("-m","--memfree", help="Memfree parameter for gnu parallel", default='40G', required=False)
