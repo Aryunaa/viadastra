7ea13b3747c83556a7f70a953edc465a8b6fda7f
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2022-10-28 21:54:08 +0300

    i

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class SmartFormatter(argparse.HelpFormatter):
 parser = argparse.ArgumentParser(description='viadastra pipeline, perfoms snp-calling, filtrating and works with babachi and mixalime',
                                  formatter_class=SmartFormatter)
 parser.add_argument("-c","--config", help="Path for cfg file, which contains all parameters", required=True)
-helpm = "Define a processing step please.\n1 creates soft links and directories\n2.1 for reference processing"\
+helpm = "R|Define a processing step please.\n1 creates soft links and directories\n2.1 for reference processing"\
         + "\n2.2.1 snp calling for one file\n2.2 snp calling for all files "
 print(helpm)
 parser.add_argument("-s","--step",choices=['1', '2.1', '2.2.1', '2.2', 'e'],help=helpm, required=True)
