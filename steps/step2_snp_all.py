98dc19f97c95a37eb11a2a9bb0d4b0647502f840
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2023-03-12 20:22:07 +0300

    debug

--- steps/step2_snp_all.py
+++ steps/step2_snp_all.py
@@ -23,7 +23,7 @@ def call_all(jobs,path,memfree):
     processing_list = os.path.join(maindir, config["Files"]["processing_list"])
 
     dir = pathlib.Path(__file__).parent.absolute()
-    script = os.path.join(dir,'steps/step2_snp_calling.py')
+    script = os.path.join(dir,'step2_snp_calling.py')
     print(script)
 
 
